0dc4c8c3ed9ac8f58052bea06932d41c7ab96903
Scott Wyatt <scottwyatt86@gmail.com>
2015-08-29 16:17:31 -0400

    Made some awesome new changes, now its even awesomer

--- src/humpback.js
+++ src/humpback.js
@@ -12,15 +12,22 @@
  */
 angular.module('humpback.core', [
   'ngTagsInput',
-  'humpback.core.cms'
+  'ui.ace',
+  'ngSanitize',
+
+  'humpback.core.users',
+  'humpback.core.routes',
+  'humpback.core.models',
+  'humpback.core.settings'
 ]);
 
 
-angular.module('humpback.core.cms', [])
+angular.module('humpback.core.users', [])
 
 .factory('Users', function(DS, utils) {
   
   var Users = function() {
+    this.visible = [];
     this.users = [];
     this.busy = false;
     this.skip = 0;
@@ -73,6 +80,7 @@ angular.module('humpback.core.cms', [])
     if (users.busy){ 
       return;
     }
+
     users.busy = true;
 
     users.start = users.skip;
@@ -80,7 +88,7 @@ angular.module('humpback.core.cms', [])
 
     console.log("SKIP:",users.skip,"START:",users.start,"END:",users.end);
 
-    DS.findAll('route', {limit: users.limit, skip: users.skip, sort: users.sort})
+    DS.findAll('user', {limit: users.limit, skip: users.skip, sort: users.sort})
     .then(function(list){
 
       users.users = _.merge(users.users, list);
@@ -96,8 +104,9 @@ angular.module('humpback.core.cms', [])
    }
   
   return Users;
-})
+});
 
+angular.module('humpback.core.routes', [])
 .factory('Routes', function(DS, utils) {
   
   var Routes = function() {
@@ -173,6 +182,86 @@ angular.module('humpback.core.cms', [])
   return Routes;
 
 
- }) 
+ });
+
+angular.module('humpback.core.models', [])
+.factory('Models', function(DS, utils) {
+  
+  var Models = function() {
+    this.visible = [];
+    this.models = [];
+    this.busy = false;
+    this.skip = 0;
+    this.limit = 10;
+    this.total = 0;
+    this.start = 0;
+    this.end = 10;
+    this.criteria = '';
+    this.sort = 'createdAt desc';
+  };
+
+  Models.prototype.prevPage = function() {
+    var models = this;
+    
+    if (models.busy){ 
+      return;
+    }
+    models.busy = true;
+
+    models.skip = models.skip - models.limit * 2 >= 0 ? models.skip - models.limit * 2 : 0;
+    models.start = models.skip;
+    models.end = models.skip + models.limit;
+
+    console.log("SKIP:",models.skip,"START:",models.start,"END:",models.end);
+
+    DS.findAll('model', {limit: models.limit, skip: models.skip, sort: models.sort})
+    .then(function(list){
+
+      models.models = _.merge(models.models, list);
+      models.visible = list;
+      
+        models.skip = models.skip + models.limit;
+        models.busy = false;
+    })
+    .catch(function(err){
+      if(utils.development()){ console.log(err); }; // reason why query failed
+    });
+
+   }
+
+  Models.prototype.nextPage = function() {
+    var models = this;
+    
+    if (models.busy){ 
+      return;
+    }
+    models.busy = true;
+
+    models.start = models.skip;
+    models.end = models.skip + models.limit;
+
+    console.log("SKIP:",models.skip,"START:",models.start,"END:",models.end);
+
+    DS.findAll('model', {limit: models.limit, skip: models.skip, sort: models.sort})
+    .then(function(list){
+
+      models.models = _.merge(models.models, list);
+      models.visible = list;
+      
+        models.skip = models.skip + models.limit;
+        models.busy = false;
+    })
+    .catch(function(err){
+      if(utils.development()){ console.log(err); }; // reason why query failed
+    });
+
+   }
+  
+  return Models;
+
+
+ });
+
+angular.module('humpback.core.settings', [])
 
 ;
